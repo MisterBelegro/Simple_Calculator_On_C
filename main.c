6a8f36e476334c5e82e43ce57ea6d5e9a6e3ad8a
MisterBelegro <119618424+MisterBelegro@users.noreply.github.com>
2023-05-02 15:51:34 +0300

    Update main.c
    Now, code is including "struct"!

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 // Variables
-int a;
-int b;
+struct variables {
+    int a;
+    int b;
+};
 
 // Math Functions
 int add(int num1, int num2) { return (num1 + num2); }
@@ -12,28 +14,33 @@ int div(int num1, int num2) { return (num1 / num2); }
 int rem(int num1, int num2) { return (num1 % num2); }
 
 // Output
-int printer(int num1, int num2, int num3, int num4, int num5, int num6, int num7) {
+void printer(int num1, int num2, int num3, int num4, int num5, int num6, int num7) {
     printf("%d + %d = %d \n", num1, num2, num3);
     printf("%d - %d = %d \n", num1, num2, num4);
     printf("%d * %d = %d \n", num1, num2, num5);
     printf("%d / %d = %d \n", num1, num2, num6);
     printf("%d %% %d = %d \n", num1, num2, num7);
-    return 0;
 }
 
 // Main Function
 int main() {
+    // Init Variables
+    struct variables vars;
+
     // Get numbers
-    scanf("%d", &a);
-    scanf("%d", &b);
+    scanf("%d", &vars.a);
+    scanf("%d", &vars.b);
+
     // Evaluate Results
-    int c1 = add(a, b);
-    int c2 = sub(a, b);
-    int c3 = mul(a, b);
-    int c4 = div(a, b);
-    int c5 = rem(a, b);
+    int c1 = add(vars.a, vars.b);
+    int c2 = sub(vars.a, vars.b);
+    int c3 = mul(vars.a, vars.b);
+    int c4 = div(vars.a, vars.b);
+    int c5 = rem(vars.a, vars.b);
+
     // ...And Print!
-    printer(a, b, c1, c2, c3, c4, c5);
+    printer(vars.a, vars.b, c1, c2, c3, c4, c5);
+    
     // Yea, And also exit Program
     return 0;
 }
